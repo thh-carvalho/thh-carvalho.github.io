faf81bf856dc0d85d1d2ed2685d0cdc758e5e105
Thiago H. Carvalho Bastos <47954204+thh-carvalho@users.noreply.github.com>
2022-11-30 00:25:31 -0300

    Site em construção

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navigation/menu/menu.component';
+import { HomeComponent } from './home/home/home.component';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
